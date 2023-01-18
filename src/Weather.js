070155cfe1e33d659a38563495fd4756a1513fa0
flaviacastnunes <117353138+flaviacastnunes@users.noreply.github.com>
2023-01-18 05:31:23 +0000

    added the weather search engine

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from 'axios';
 
-export default function Weather(props){
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=a1140261bdb0b194c8ae933d2f478860&units=metric`;
-    
-
-    function showTemp(response){
-alert(`It's ${response.data.main.temp}ºC`)
+export default function Weather() {
+    const [city, setCity] = useState("");
+  
+    function updateCity(event) {
+      event.preventDefault();
+      setCity(event.target.value);
     }
-axios.get(url).then(showTemp);
-}
+    function handleSubmit(event) {
+      event.preventDefault();
+      let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a1140261bdb0b194c8ae933d2f478860&units=metric`;
+      axios.get(url).then(showData);
+    }
+  
+    let [temperature, setTemperature] = useState("");
+    let [description, setDescription] = useState("");
+    let [humidity, setHumidity] = useState("");
+    let [wind, setWind] = useState("");
+    let [icon, setIcon] = useState("");
+  
+    function showData(response) {
+      console.log(response);
+      setTemperature(`Temperature: ${Math.round(response.data.main.temp)}ºC`);
+      setDescription(`Description: ${response.data.weather[0].description}`);
+      setHumidity(`Humidity: ${Math.round(response.data.main.humidity)}%`);
+      setWind(`Wind: ${Math.round(response.data.wind.speed)}km/h`);
+      setIcon(
+        `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      );
+    }
+  
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter a city..."
+            onChange={updateCity}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <ul>
+          <li>{temperature}</li>
+          <li>{description}</li>
+          <li>{humidity}</li>
+          <li>{wind}</li>
+          <li>
+            <img src={icon} alt="icon" />
+          </li>
+        </ul>
+        <div>Coded by Flávia Nunes, with open source on <a href="" tittle="GitHub Repository">GitHub</a></div>
+      </div>
+    );
+  }
